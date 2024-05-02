4d61ac8bcd7f28c11306ea519048f874c0562c66
Mor-uto <morutomc@gmail.com>
2024-05-02 22:37:10 +0300

    removed the unregister method

--- src/main/java/net/moruto/morutosframework/listener/MListener.java
+++ src/main/java/net/moruto/morutosframework/listener/MListener.java
@@ -1,10 +1,5 @@
 package net.moruto.morutosframework.listener;
 
-import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 
-public interface MListener extends Listener {
-    default void unregister() {
-        HandlerList.unregisterAll(this);
-    }
-}
+public interface MListener extends Listener { }
