e38c25a30b0806a1b3cd0186bef18ee8e7055f2f
Mor-uto <morutomc@gmail.com>
2024-05-05 20:12:30 +0300

    Added simpler http requests

---
+++ src/main/java/net/moruto/morutosframework/http/HttpRequestType.java
@@ -0,0 +1,13 @@
+package net.moruto.morutosframework.http;
+
+public enum HttpRequestType {
+    POST,
+    GET,
+    DELETE,
+    HEAD,
+    PUT,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH
+}
---
+++ src/main/java/net/moruto/morutosframework/http/HttpResponse.java
@@ -0,0 +1,3 @@
+package net.moruto.morutosframework.http;
+
+public record HttpResponse(String response, int responseCode) {}
